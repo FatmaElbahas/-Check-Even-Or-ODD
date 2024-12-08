923850013969e42d68d5333ca8d54dc2de7b4c6e
PASSWOED <PASSWOED@DESKTOP-UI80RJC>
2024-12-08 17:44:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp113
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Check Even Or ODD
+            Console.WriteLine("Enter The Number : ");
+            float num=Convert.ToSingle(Console.ReadLine());
+            if(num%2==0)
+            {
+                Console.WriteLine("The Number Is Even");
+            }
+            else
+            {
+                Console.WriteLine("The Number Is Odd");
+            }
+            #endregion
+        }
+    }
+}
